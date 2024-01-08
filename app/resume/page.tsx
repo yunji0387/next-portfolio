a4fa075837f4da74475eff897951211404c96aeb
yunji0387 <yunji0387@gmail.com>
2024-01-07 20:33:11 -0600

    Add resume page

---
+++ app/resume/page.tsx
@@ -0,0 +1,20 @@
+import { Footer } from "../components/Footer";
+
+export default function Resume() {
+  return (
+    <div className="custom-container h-[80vh]">
+      <div className="bg-red-900 flex flex-col justify-center items-center w-full h-full">
+        <p className="text-white text-sm">{'>'} Resume</p>
+        <object
+          data="/resume.pdf"
+          type="application/pdf"
+          width="100%"
+          height="100%"
+        >
+          <p>Unable to display PDF file, please try again later.</p>
+        </object>
+      </div>
+      <Footer />
+    </div>
+  );
+}
