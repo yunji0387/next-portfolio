f781badee1f9377a75b7e878d72c0805ab26c059
yunji0387 <yunji0387@gmail.com>
2024-01-12 14:15:14 -0600

    Remove commented code and add "Home" link to Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,42 +1,13 @@
-// import Link from 'next/link';
-
-// const navItems = [
-//     {
-//         name: 'Projects',
-//         link: '/projects'
-//     },
-//     {
-//         name: 'Contact',
-//         link: '/contact'
-//     },
-//     {
-//         name: 'About',
-//         link: '/about'
-//     },
-// ];
-
-// export function Navbar() {
-//     return (
-//         <nav className="w-full">
-//             <ul className="bg-cyan-800 flex items-center justify-center p-2 space-x-4 h-11">
-//                 {navItems.map((item) => (
-//                     <li key={item.name}>
-//                         <Link href={item.link}>
-//                             <p className="p-2 text-white hover:text-lg transition-all duration-300 ease-in-out">{item.name}</p>
-//                         </Link>
-//                     </li>
-//                 ))}
-//             </ul>
-//         </nav>
-//     );
-// }
-
 "use client";
 import React, { useState } from "react";
 import Link from "next/link";
 import { HamburgerIcon } from "./HamburgerIcon";
 
 const navItems = [
+  {
+    name: "Home",
+    link: "/",
+  },
   {
     name: "Projects",
     link: "/projects",
