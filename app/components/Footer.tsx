393612aad43811beb8a0c7ead317b1ffc171dd79
yunji0387 <yunji0387@gmail.com>
2024-01-12 16:08:08 -0600

    Refactor Footer component to use Image component for icons

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,37 +1,55 @@
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
+// import GitHubIcon from '/icons/github.png';
+// import LinkedInIcon from '/icons/linkedin.png';
+// import TwitterIcon from '/icons/twitter.png';
+// import GmailIcon from '/icons/gmail.png';
 
 const footerItems = [
   {
     name: "github",
     link: "https://github.com/yunji0387",
+    iconPath: "/icons/github.png",
   },
   {
     name: "linkedin",
     link: "https://www.linkedin.com/in/yun-ji-how-64025220a/",
+    iconPath: "/icons/linkedin.png",
   },
   {
     name: "email",
     link: "mailto: yunji0387@gmail.com",
+    iconPath: "/icons/gmail.png",
   },
   {
     name: "X",
     link: "https://twitter.com/",
+    iconPath: "/icons/twitter.png",
   },
 ];
 
 export function Footer() {
   return (
-    <footer className="flex flex-col justify-center items-center w-full">
-      <ul className="bg-pink-800 flex items-center justify-center">
+    <footer className="flex flex-col justify-center items-center w-full mt-2">
+      <ul className="flex flex-row items-center justify-center">
         {footerItems.map((item) => (
-          <li key={item.name} className="p-2">
-            <Link href={item.link} target="_blank" rel="noopener noreferrer">
-              <p className="text-white hover:underline">{item.name}</p>
+          <li
+            key={item.name}
+            className="flex items-center justify-center m-1 p-2 w-14"
+          >
+            <Link href={item.link} target="_blank" rel="noopener noreferrer" className="flex flex-col items-center justify-center">
+              <Image
+                src={item.iconPath}
+                alt={item.name}
+                width={50}
+                height={50}
+                className="hover:scale-150 transition-all duration-300 ease-in-out"
+              />
             </Link>
           </li>
         ))}
       </ul>
-      <p>&copy; {new Date().getFullYear()} Yun Ji How.</p>
+      <p className="text-sm select-none">&copy; {new Date().getFullYear()} Yun Ji How.</p>
     </footer>
   );
 }
