841828ef91b64f7711b2678291fc561a0222bd10
yunji0387 <yunji0387@gmail.com>
2024-01-12 14:27:02 -0600

    Refactor HamburgerIcon styling

--- app/components/HamburgerIcon.tsx
+++ app/components/HamburgerIcon.tsx
@@ -7,22 +7,22 @@ export function HamburgerIcon({ isOpen, handleClick }: HamburgerIconProps) {
   return (
     <button
       onClick={handleClick}
-      className="bg-pink-600 flex flex-col justify-center items-center w-6 h-6"
+      className="flex flex-col justify-center items-center w-12 h-12 border border-2 rounded-md"
     >
       <span
-        className={`bg-blue-600 bg-steel-500 block transition-all duration-300 ease-out 
+        className={`bg-white bg-steel-500 block transition-all duration-300 ease-out
                     h-0.5 w-6 rounded-sm ${
                       isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
                     }`}
       ></span>
       <span
-        className={`bg-blue-600 bg-steel-500 block transition-all duration-300 ease-out 
+        className={`bg-white bg-steel-500 block transition-all duration-300 ease-out
                     h-0.5 w-6 rounded-sm my-0.5 ${
                       isOpen ? "opacity-0" : "opacity-100"
                     }`}
       ></span>
       <span
-        className={`bg-blue-600 bg-steel-500 block transition-all duration-300 ease-out 
+        className={`bg-white bg-steel-500 block transition-all duration-300 ease-out
                     h-0.5 w-6 rounded-sm ${
                       isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
                     }`}
