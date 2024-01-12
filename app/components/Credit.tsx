319bbf31cc0fb6c9aa94ade9920ddfa552112821
yunji0387 <yunji0387@gmail.com>
2024-01-12 15:41:12 -0600

    Refactor code for improved performance and readability

---
+++ app/components/Credit.tsx
@@ -0,0 +1,46 @@
+export function Credit() {
+  return (
+    <div>
+      <p>
+        Github icons created by
+        <a
+          href="https://www.flaticon.com/free-icons/github"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ruslan Babkin - Flaticon
+        </a>
+      </p>
+      <p>
+        Linkedin icons created by
+        <a
+          href="https://www.flaticon.com/free-icons/linkedin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          riajulislam - Flaticon
+        </a>
+      </p>
+      <p>
+        Twitter icons created by
+        <a
+          href="https://www.flaticon.com/free-icons/twitter"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Pixel perfect - Flaticon
+        </a>
+      </p>
+      <p>
+        Gmail icons created by
+        <a
+          href="https://www.flaticon.com/free-icons/gmail"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Pixel perfect - Flaticon
+        </a>
+      </p>
+    </div>
+  );
+}
