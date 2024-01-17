d60f76fcafcc57334c5b0a3f68aa969a7d8c428d
yunji0387 <yunji0387@gmail.com>
2024-01-16 19:38:18 -0600

    Add new icons for MUI, Java, Linux, Jupyter, VSCode, GitHub, Vercel, WordPress, Figma, and Godot

--- public/infos/skillsIconList.tsx
+++ public/infos/skillsIconList.tsx
@@ -16,6 +16,10 @@ export const skillsIconList = [
         icon: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg",
     },
     {
+        title: "MUI",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
+    },
+    {
         title: "Node.js",
         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
     },
@@ -35,5 +39,40 @@ export const skillsIconList = [
         title: "Heroku",
         icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original.svg",
     },
-    
+    {
+        title: "Java",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+    },
+    {
+        title: "Linux",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg",
+    },
+    {
+        title: "Jupyter",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jupyter/jupyter-original.svg",
+    },
+    {
+        title: "VSCode",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+    },
+    {
+        title: "GitHub",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+    },
+    {
+        title: "Vercel",
+        icon: "/icons/vercel.ico",
+    },
+    {
+        title: "WordPress",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-original.svg",
+    },
+    {
+        title: "Figma",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+    },
+    {
+        title: "Godot",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/godot/godot-original.svg",
+    }
 ];
