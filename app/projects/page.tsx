1367cca6f860470b63852a429a81eebb75b2b40f
yunji0387 <yunji0387@gmail.com>
2024-01-28 14:32:11 -0600

    Refactor project card layout in Projects component

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -16,17 +16,11 @@ export default function Projects() {
             {projectList.map((project, index) => (
               <div
                 key={index}
-                // className="w-full py-2 my-2 border-b-2 border-dashed border-black dark:border-white bg-amber-300"
                 className="flex flex-col w-full p-2 my-2 bg-amber-200 dark:bg-gray-700"
               >
-                <p className="pl-1 font-semibold">
-                  {project.name}
-                </p>
-                {/* <div className="grid grid-cols-12 gap-1"> */}
+                <p className="pl-1 font-semibold">{project.name}</p>
                 <div className="flex flex-col sm:flex-row gap-1">
-                {/* <div className="col-span-12 md:col-span-4 lg:col-span-3 flex justify-center items-center w-full"> */}
                   <div className="flex justify-center items-center w-full sm:w-60 sm:min-w-60 sm:h-36">
-                    {/* <div className="w-full max-w-80 m-2 terminal-outer-container border"> */}
                     <div className="flex w-full sm:max-w-60 sm:h-36">
                       <Image
                         src={project.image}
@@ -34,15 +28,18 @@ export default function Projects() {
                         width={0}
                         height={0}
                         layout="responsive"
-                        // className="select-none w-full h-auto"
                         className="select-none w-full h-auto sm:w-60 sm:h-36 p-1"
                       />
                     </div>
                   </div>
                   <div className="w-full mt-2">
-                  {/* <div className="col-span-12 md:col-span-8 lg:col-span-9 mt-2"> */}
-                    <p className="text-xs sm:text-sm md:text-base text-justify px-2">
+                    {/* <p className="text-xs sm:text-sm md:text-base text-justify px-2">
                       {project.description}
+                    </p> */}
+                    <p className="text-xs sm:text-sm md:text-base text-justify px-2">
+                      {project.description.length > 100
+                        ? `${project.description.substring(0, 100)}...`
+                        : project.description}
                     </p>
                     <div className="flex flex-col sm:flex-row p-1 text-sm md:text-base font-semibold underline">
                       <Link
