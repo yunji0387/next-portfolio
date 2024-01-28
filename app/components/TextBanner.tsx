d32141b4fd3c0606c96367ed27bffa605ba0ee51
yunji0387 <yunji0387@gmail.com>
2024-01-28 03:13:06 -0600

    Update TextBanner component styling

--- app/components/TextBanner.tsx
+++ app/components/TextBanner.tsx
@@ -11,7 +11,8 @@ export function TextBanner({ textList, speed = 8000 }: TextBannerProps) {
   return (
     <div className="flex flex-row items-center justify-center">
       <div className="relative overflow-hidden h-7 w-[98%] text-sm">
-        <div className="absolute flex whitespace-nowrap items-center justify-center w-full h-full bg-amber-800 dark:bg-indigo-900 border-y border-black dark:border-white text-white">
+        {/* <div className="absolute flex whitespace-nowrap items-center justify-center w-full h-full bg-amber-800 dark:bg-indigo-900 border-y border-black dark:border-white text-white"> */}
+        <div className="absolute flex whitespace-nowrap items-center justify-center w-full h-full bg-amber-100 dark:bg-gray-800 text-black dark:text-white font-bold">
           <section className="scroll-section" style={scrollStyle}>
             {textList.map((text, index) => (
               <p key={index} className="p-1 w-full">
