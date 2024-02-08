de1ada5c9996e0927f7921c71ddb581c5106f34b
yunji0387 <yunji0387@gmail.com>
2024-02-08 17:55:40 -0600

    Refactor about page layout and styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,10 +13,14 @@ export default function About() {
       <Navbar />
       <div className="flex flex-col justify-center items-center w-full gap-2">
         <div className="flex flex-col sm:flex-row gap-2 w-full">
-          <div className="w-full terminal-outer-container">
-            <div className="terminal-inner-container w-full h-full">
-              <p className="font-black text-lg sm:text-2xl select-none">&#128020; About Me</p>
-              <p className="text-sm md:text-base lg:text-lg text-justify sm:p-3">{about.description}</p>
+          <div className="flex flex-col w-full sm:w-[50%]">
+            <p className="font-black text-lg sm:text-2xl select-none">
+              &#128020; About Me
+            </p>
+            <div className="styled-card w-full h-full my-2 p-5">
+              <p className="text-sm md:text-base lg:text-lg text-justify sm:p-3">
+                {about.description}
+              </p>
               <div className="flex flex-row h-6 text-sm md:text-base lg:text-lg">
                 <p>&#10148; Check out my&nbsp;</p>
                 <Link href="/resume" rel="noopener noreferrer">
@@ -29,9 +33,11 @@ export default function About() {
             </div>
           </div>
 
-          <div className="w-full terminal-outer-container">
-            <div className="w-full h-full terminal-inner-container">
-              <p className="font-black text-lg sm:text-2xl select-none">&#127891; Education</p>
+          <div className="flex flex-col w-full sm:w-[50%]">
+            <p className="font-black text-lg sm:text-2xl select-none">
+              &#127891; Education
+            </p>
+            <div className="styled-card w-full h-full my-2 p-5">
               {education.map((edu) => (
                 <div
                   key={edu.school}
@@ -48,57 +54,60 @@ export default function About() {
           </div>
         </div>
 
-        <div className="w-full terminal-outer-container">
-          <div className="w-full terminal-inner-container">
-            <p className="font-black text-lg sm:text-2xl select-none">&#10024; Work Experience</p>
-            {workExperience.map((work) => (
-              <div key={work.company} className="bg-amber-100 dark:bg-gray-700 bg-opacity-90 dark:bg-opacity-60 w-full my-2 p-1 sm:p-3 text-sm md:text-base lg:text-lg shadow-lg">
-                <p className="font-bold">{work.company}</p>
-                <p className="font-semibold">
-                  {work.title}{" "}
-                  <span className="font-light">({work.jobType})</span>
-                </p>
-                <p className="font-light">{work.date}</p>
-                <p className="">{work.location}</p>
-                <p className="font-extralight">{work.description}</p>
-              </div>
-            ))}
-          </div>
+        <div className="w-full">
+          <p className="font-black text-lg sm:text-2xl select-none">
+            &#10024; Work Experience
+          </p>
+          {workExperience.map((work) => (
+            <div
+              key={work.company}
+              className="styled-card w-full my-2 p-5 text-sm md:text-base lg:text-lg"
+            >
+              <p className="font-bold">{work.company}</p>
+              <p className="font-semibold">
+                {work.title}{" "}
+                <span className="font-light">({work.jobType})</span>
+              </p>
+              <p className="font-light">{work.date}</p>
+              <p className="">{work.location}</p>
+              <p className="font-extralight">{work.description}</p>
+            </div>
+          ))}
         </div>
 
-        <div className="w-full terminal-outer-container">
-          <div className="w-full terminal-inner-container">
-            <p className="font-black text-lg sm:text-2xl select-none">&#129495; Certifications</p>
-            <div className="overflow-auto max-h-64">
-              {certificationsList.map((cert) => (
-                <Link
-                  key={cert.name}
-                  href={cert.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <div className="flex flex-row w-full items-center p-1 sm:p-2 text-sm md:text-base hover:bg-amber-300 dark:hover:bg-gray-700 font-semibold hover:font-black transition-all duration-300 ease-in-out">
-                    <div className="hidden sm:block">
-                      <Image
-                        src={cert.image}
-                        alt={cert.organization}
-                        width={50}
-                        height={50}
-                        className="select-none m-1 mr-2"
-                      />
-                    </div>
-                    <div>
-                      <p className="hidden sm:block">
-                        {cert.name} - {cert.organization}
-                      </p>
-                      <p className="sm:hidden">{cert.name}</p>
-                      <p className="sm:hidden">{cert.organization}</p>
-                      <p>{cert.date}</p>
-                    </div>
+        <div className="w-full">
+          <p className="font-black text-lg sm:text-2xl select-none">
+            &#129495; Certifications
+          </p>
+          <div className="styled-card my-2 p-3 overflow-auto max-h-64">
+            {certificationsList.map((cert) => (
+              <Link
+                key={cert.name}
+                href={cert.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="flex flex-row w-full items-center p-1 sm:p-2 text-sm md:text-base hover:bg-amber-50 dark:hover:bg-gray-700 bg-opacity-90 font-bold hover:font-black transition-all duration-300 ease-in-out">
+                  <div className="hidden sm:block">
+                    <Image
+                      src={cert.image}
+                      alt={cert.organization}
+                      width={50}
+                      height={50}
+                      className="select-none m-1 mr-2"
+                    />
                   </div>
-                </Link>
-              ))}
-            </div>
+                  <div>
+                    <p className="hidden sm:block">
+                      {cert.name} - {cert.organization}
+                    </p>
+                    <p className="sm:hidden">{cert.name}</p>
+                    <p className="font-normal sm:hidden">{cert.organization}</p>
+                    <p className="font-light">{cert.date}</p>
+                  </div>
+                </div>
+              </Link>
+            ))}
           </div>
         </div>
       </div>
