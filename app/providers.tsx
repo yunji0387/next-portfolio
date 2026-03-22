f678892c92793b8262f154aa8ac716add91c5737
yunji0387 <yunji0387@gmail.com>
2026-03-22 23:14:11 +0800

    fix: add suppressHydrationWarning to RootLayout and simplify Providers component

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,22 +1,11 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
-import { useEffect, useState } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return <>{children}</>
-  };
-
   return (
-      <ThemeProvider attribute="class" defaultTheme="dark">
-        {children}
-      </ThemeProvider>
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      {children}
+    </ThemeProvider>
   );
 }
